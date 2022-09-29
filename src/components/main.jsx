fd78220a23c908415e314790ec77e9e87b00bf4a
Virenk21 <viren.d222111@gmail.com>
2022-09-29 23:27:42 +0530

    updated mainjs

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const main = () => {
+    const [movies, setMovies] = useState([]);
   return (
     <div>main</div>
+      
   )
 }
 
