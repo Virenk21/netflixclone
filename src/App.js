1ce21aecb6ee87039b368d5d3adbb3982f07757a
Virenk21 <viren.d222111@gmail.com>
2022-09-29 17:07:19 +0530

    navbar updated with logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className="text-white">Netflix C</h1>
+     <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
+    <div>
+    <button>Sign In</button>
+    <button>Sign Up</button>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
