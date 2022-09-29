1122582908bc3c49ce0d1574c6571646e47078e5
Virenk21 <viren.d222111@gmail.com>
2022-09-29 15:37:22 +0530

    dependencies and plugins added

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+  plugins: [require("tailwind-scrollbar-hide")],
+};
