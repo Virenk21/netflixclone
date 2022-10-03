bdfd73c916ca93932b07769296899fd40797ba1f
Virenk21 <viren.d222111@gmail.com>
2022-10-03 22:19:22 +0530

    scroll functionality updated

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Movie from './Movie';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 
 const Row = ({title, fetchURL}) => {
@@ -14,18 +15,20 @@ const Row = ({title, fetchURL}) => {
     });
     },[fetchURL]);
 
-    console.log(movies)
-
-
+    
   return (
     <>
     <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
     <div className='relative flex items-center group'>
-    <div id={'slider'}>
+      <MdChevronLeft className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}/>
+    <div id={'slider'}  className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
          {movies.map((item, id) => (
             <Movie key={id} item={item}/>
          ))}
     </div>
+    <MdChevronRight className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}/>
     </div>
     </>
   )
