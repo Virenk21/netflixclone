816ab752e8fa18107d999a23d41effb5d22fc588
Virenk21 <viren.d222111@gmail.com>
2022-10-03 07:45:54 +0530

    further updated rowjsx

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -22,8 +22,10 @@ const Row = ({title, fetchURL}) => {
     <div id={'slider'}>
          {movies.map((item, id) => (
             <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-                <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
-                
+                <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
+            <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+            <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
+            </div>
             </div>
 
          ))}
