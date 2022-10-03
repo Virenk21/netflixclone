927b951a1ab48b6c7a69a00fd1be6f42c598a1fb
Virenk21 <viren.d222111@gmail.com>
2022-10-03 23:56:56 +0530

    updated appjs

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
