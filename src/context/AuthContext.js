026e3bc671f869f2aa4d429f2b9fa350103f4ee6
Virenk21 <viren.d222111@gmail.com>
2022-10-03 23:59:09 +0530

    updated autocontextjs

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,11 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 
 const AuthContext = createContext();
